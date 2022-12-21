634ba1e90009fff4e526e829e963101381d2cdd7
Glazed_Belmont <48196637+GlaZedBelmont@users.noreply.github.com>
2022-12-20 20:10:33 -0500

    remade by Alka

--- fedora-script.sh
+++ fedora-script.sh
@@ -1,47 +1,24 @@
 #!/bin/bash
 
+
+printf 'This script needs to be run as superuser (root on most unix systems)\n'
+cd ~/Desktop/
+
 sudo su - root << EOF
-printf 'Beginning installation\n'
-EOF
+echo "max_parallel_downloads=10\ndeltarpm=true" >> /etc/dnf/dnf.conf
 
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm;
 
-# Make DNF actually usable
-echo -e "max_parallel_downloads=10\ndeltarpm=true" >> test.txt
+dnf update --refresh -y;
 
-# Install the RPM Fusion repos
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin sound-and-video;
+dnf group upgrade --with-optional Multimedia;
+dnf install -y tlp tlp-rdw lame\* --exclude=lame-devel intel-media-driver ffmpeg-libs libva libva-utils git;
+sudo systemctl mask power-profiles-daemon;
 
-# Install app-stream metadata
-sudo dnf groupupdate core
-
-
-# Install TLP
-sudo dnf install -y tlp tlp-rdw
-
-# Mask power-profiles-daemon
-sudo systemctl mask power-profiles-daemon
-
-
-# Media Codecs
-
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-sudo dnf groupupdate sound-and-video
-sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
-sudo dnf install lame\* --exclude=lame-devel
-sudo dnf group upgrade --with-optional Multimedia
-
-
-# HW VAAPI
-sudo dnf -y install intel-media-driver ffmpeg-libs libva libva-utils
-
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak update
-
-sudo dnf install -y git
-
-cd /home/$USER
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; flatpak update
 
 git clone https://github.com/eupnea-linux/audio-scripts
-cd audio-scripts
-./setup-audio
+cd audio-scripts;chmod+x setup-audio;./setup-audio
+EOF
+printf 'Installation finished, please reboot your system\n'
